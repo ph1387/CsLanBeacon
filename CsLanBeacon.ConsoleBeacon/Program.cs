850ff09bfbee208cc95687d744d385750ff0d1d3
P H <ph1387@t-online.de>
2018-07-29 19:26:41 +0200

    Added console implementations of probe and beacon

---
+++ CsLanBeacon.ConsoleBeacon/Program.cs
@@ -0,0 +1,25 @@
+﻿using CsLanBeacon.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsLanBeacon.ConsoleBeacon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var beacon = new Beacon("CustomKey");
+
+            beacon.BeaconActiveEvent += (s, e) => { Console.WriteLine("Beacon active."); };
+            beacon.BeaconStoppedEvent += (s, e) => { Console.WriteLine("Beacon stopped."); };
+            beacon.BeaconBroadcastReceivedEvent += (s, e) => { Console.WriteLine("Broadcast received."); };
+            beacon.BeaconRespondEvent += (s, e) => { Console.WriteLine("Sending response."); };
+            beacon.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CsLanBeacon.ConsoleProbe/Program.cs
@@ -0,0 +1,25 @@
+﻿using CsLanBeacon.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsLanBeacon.ConsoleProbe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var beacon = new Probe("CustomKey", TimeSpan.FromSeconds(1));
+
+            beacon.ProbeActiveEvent += (s, e) => { Console.WriteLine("Probe active."); };
+            beacon.ProbeStoppedEvent += (s, e) => { Console.WriteLine("Probe stopped."); };
+            beacon.ProbeBroadcastEvent += (s, e) => { Console.WriteLine("Sending broadcast."); };
+            beacon.ProbeReceivedResponseEvent += (s, e) => { Console.WriteLine("Received response."); };
+            beacon.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
